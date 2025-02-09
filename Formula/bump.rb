f071ae90d93b7c02c4329f163c3738f35a100640
Matt Hadley <hello@matthadley.me>
2025-02-08 18:01:57 -0800

    fix url

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -2,7 +2,7 @@ class Bump < Formula
   desc "Version management CLI for multiple languages"
   homepage "https://github.com/mattresspadley/bump"
   version "0.0.4"
-  url "https://github.com/mattresspadley/bump/releases/download/v0.0.4/bump-macos"
+  url "https://github.com/mattresspadley/bump/releases/download/v0.0.4/bump-macos.zip"
   sha256 "REPLACE_WITH_YOUR_SHA256"
   license "MIT"
 
