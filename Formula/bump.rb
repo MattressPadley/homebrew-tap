9681ca9a3bcd5aa508c96b13ded6f145166543af
Matt Hadley <hello@matthadley.me>
2025-02-08 18:29:35 -0800

    new bump version

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -1,8 +1,8 @@
 class Bump < Formula
   desc "Version management CLI for multiple languages"
   homepage "https://github.com/mattresspadley/bump"
-  version "0.0.5"
-  url "https://github.com/MattressPadley/bump/releases/download/v0.0.5/bump-macos-v0.0.5.tar.gz"
+  version "0.0.6"
+  url "https://github.com/MattressPadley/bump/releases/download/v0.0.6/bump-macos-v0.0.6.tar.gz"
   sha256 "5610d778870306fc1d0c3cba5a607293025a590f201a702ec09f17edcf9aa736"
   license "MIT"
 
